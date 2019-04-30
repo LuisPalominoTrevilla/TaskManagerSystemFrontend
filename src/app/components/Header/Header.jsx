11772a2118f37a1246e17c1b25ab0d0c4168f3f9
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-30 16:06:46 -0500

    header

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.scss'
+
+class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            images: [
+              'https://avatarfiles.alphacoders.com/865/86573.jpg'
+          ],
+        }
+      }
+    render() {
+        return (
+            <div className="header-contain">
+                <div className="title">T</div>
+                <img src={this.state.images[0]} className="avatar"/>
+            </div>
+        )
+    }
+
+}
+
+export default Header;
