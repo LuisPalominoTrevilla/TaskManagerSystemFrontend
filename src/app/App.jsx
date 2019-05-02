3f5517186b57c1b00af61040944d4ea1af96e847
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-05-01 20:00:08 -0500

    added the redirection and aded global file which Palomino will hate

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import Login from './components/Login/Login.js';
 import Register from './components/Register/Register.js';
 import './App.scss';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import HabitPage from './components/HabitPage/HabitPage';
 import TaskPage from './components/TaskPage/TaskPage';
 import ReportPage from './components/ReportPage/ReportPage';
+import './global.js';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+        <Route path='/' exact render = {() => (
+          global.user != '' ? <Redirect to='/habits'/> : <Redirect to='/login'/> 
+        )}/>
         <Route path='/habits' exact component={HabitPage}/>
         <Route path='/tasks' exact component={TaskPage}/>
         <Route path='/report' exact component={ReportPage}/>
