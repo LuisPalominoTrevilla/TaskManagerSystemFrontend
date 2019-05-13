bf88bcd753603b9f2241de4093d8241e92df6f56
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-05-12 23:43:17 -0500

    functional LogIn

--- src/app/actions/account.js
+++ src/app/actions/account.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-export function accountLogin() {
-    axios.get('').then(
-        (res) => {
-            console.log(res.data);
-        }
-    )
+export function accountLogin(email01, password01) {
+    return (axios.post('http://localhost:3000/api/v1/accounts/login',
+    {
+        email: email01,
+        password: password01
+    }
+    ));
 }
